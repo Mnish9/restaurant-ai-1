6ff8321232791a95ee39613c24a2cbd3ef5c6db5
Manish Mewara <itzmanish9@gmail.com>
2024-05-03 18:44:21 +0530

    add new apis

--- src/App.ts
+++ src/App.ts
@@ -6,11 +6,12 @@ import swaggerJSDoc from "swagger-jsdoc";
 import { swaggerDefinition } from "./swagger/swagger";
 
 import connectToDb from "./db/connection";
-// import adminRoutes from "./features/admin/routes";
-// import authRoutes from "./features/auth/routes";
-// import categoryRoutes from "./features/categories/routes";
 import * as Sentry from "@sentry/node";
 import envConfig from "./config/envConfig";
+import restaurantRoutes from "./features/restaurant/routes";
+import MainCategoriesRoutes from "./features/main-categories/routes";
+import CategoriesRoutes from "./features/categories/routes";
+import ItemsRoutes from "./features/items/routes";
 
 const swaggerUi = require("swagger-ui-express");
 
@@ -30,7 +31,10 @@ connectToDb();
 
 // app.use("/api/v1/auth", authRoutes);
 // app.use("/api/v1/admin", adminRoutes);
-// app.use("/api/v1/category", categoryRoutes);
+app.use("/api/v1/restaurant", restaurantRoutes);
+app.use("/api/v1/main_category", MainCategoriesRoutes);
+app.use("/api/v1/category", CategoriesRoutes);
+app.use("/api/v1/items", ItemsRoutes);
 
 const swaggerSpec = swaggerJSDoc(swaggerDefinition);
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
